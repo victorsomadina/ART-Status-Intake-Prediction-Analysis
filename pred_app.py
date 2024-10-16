a85a4884a85d824f94c17667908a3d9c7e94d5d7
victorsomadina <christopher.somadina.1999@gmail.com>
2024-10-16 09:25:44 +0100

    updated

--- pred_app.py
+++ pred_app.py
@@ -14,7 +14,7 @@ st.image(r'C:\Users\hp\Downloads\Current-State-of-HIV-and-AIDS-Treatment_pv.jpg'
           caption='HIV/AIDS ART Intake Prediction', use_column_width=True)
 
 # Load the ART Dataset
-ART_Data = pd.read_csv(r'C:\Users\hp\Downloads\ART_data.xls')
+ART_Data = pd.read_csv(r'C:\Users\hp\Downloads\ART Project\ART-Status-Intake-Prediction-Analysis\new_ART_data.csv')
 X = ART_Data.drop(columns='CurrentARTStatus')
 Y = ART_Data['CurrentARTStatus'] 
 
@@ -23,40 +23,58 @@ Y = ART_Data['CurrentARTStatus']
 st.sidebar.header('Specify Input Parameters')
 
 def user_input_features():
-    current_Age = st.sidebar.slider('current_Age', float(X.current_Age.min()), float(X.current_Age.max()), float(X.current_Age.mean()))
-    Age_At_Start = st.sidebar.slider('Age_At_Start', float(X.Age_At_Start.min()), float(X.Age_At_Start.max()), float(X.Age_At_Start.mean()))
+    # Continuous Features
+    Days_To_Schedule = st.sidebar.slider('Days_To_Schedule', int(X.Days_To_Schedule.min()), int(X.Days_To_Schedule.max()), int(X.Days_To_Schedule.mean()))
+    CurrentViralLoad = st.sidebar.slider('CurrentViralLoad', float(X.CurrentViralLoad.min()), float(X.CurrentViralLoad.max()), float(X.CurrentViralLoad.mean()))
     DaysOfARVRefill = st.sidebar.slider('DaysOfARVRefill', float(X.DaysOfARVRefill.min()), float(X.DaysOfARVRefill.max()), float(X.DaysOfARVRefill.mean()))
-    RegimenAtARTStart = st.sidebar.slider('RegimenAtARTStart', float(X.RegimenAtARTStart.min()), float(X.RegimenAtARTStart.max()), float(X.RegimenAtARTStart.mean()))
-    CurrentARTRegimen = st.sidebar.slider('CurrentARTRegimen', float(X.CurrentARTRegimen.min()), float(X.CurrentARTRegimen.max()), float(X.CurrentARTRegimen.mean()))
-    CurrentWeight_Kg = st.sidebar.slider('CurrentWeight_Kg', float(X.CurrentWeight_Kg.min()), float(X.CurrentWeight_Kg.max()), float(X.CurrentWeight_Kg.mean()))
     CurrentHeight_cm = st.sidebar.slider('CurrentHeight_cm', float(X.CurrentHeight_cm.min()), float(X.CurrentHeight_cm.max()), float(X.CurrentHeight_cm.mean()))
-    Sex_Female = st.sidebar.slider('Sex_Female', float(X.Sex_Female.min()), float(X.Sex_Female.max()), float(X.Sex_Female.mean()))
-    Sex_Male = st.sidebar.slider('Sex_Male', float(X.Sex_Male.min()), float(X.Sex_Male.max()), float(X.Sex_Male.mean()))
-    Biometric_Status_No = st.sidebar.slider('Biometric_Status_No', float(X.Biometric_Status_No.min()), float(X.Biometric_Status_No.max()), float(X.Biometric_Status_No.mean()))
-    Biometric_Status_Yes = st.sidebar.slider('Biometric_Status_Yes', float(X.Biometric_Status_Yes.min()), float(X.Biometric_Status_Yes.max()), float(X.Biometric_Status_Yes.mean()))
-    ViralLoadIndication_Initial = st.sidebar.slider('ViralLoadIndication_Initial', float(X.ViralLoadIndication_Initial.min()), float(X.ViralLoadIndication_Initial.max()), float(X.ViralLoadIndication_Initial.mean()))
-    ViralLoadIndication_Normal_priority_status = st.sidebar.slider('ViralLoadIndication_Normal_priority_status', float(X['ViralLoadIndication_Normal_priority_status'].min()), float(X['ViralLoadIndication_Normal_priority_status'].max()), float(X['ViralLoadIndication_Normal_priority_status'].mean()))
-    Appointment_Status_Active_With_Drugs = st.sidebar.slider('Appointment_Status_Active_With_Drugs', float(X['Appointment_Status_Active_With_Drugs'].min()), float(X['Appointment_Status_Active_With_Drugs'].max()), float(X['Appointment_Status_Active_With_Drugs'].mean()))
-    Appointment_Status_LTFU = st.sidebar.slider('Appointment_Status_LTFU', float(X.Appointment_Status_LTFU.min()), float(X.Appointment_Status_LTFU.max()), float(X.Appointment_Status_LTFU.mean()))
-    Appointment_Status_Missed_Appointment= st.sidebar.slider('Appointment_Status_Missed_Appointment', float(X['Appointment_Status_Missed_Appointment'].min()), float(X['Appointment_Status_Missed_Appointment'].max()), float(X['Appointment_Status_Missed_Appointment'].mean()))
-    data = {'current_Age': current_Age,
-            'Age_At_Start': Age_At_Start,
-            'DaysOfARVRefill': DaysOfARVRefill,
-            'RegimenAtARTStart': RegimenAtARTStart,
-            'CurrentARTRegimen': CurrentARTRegimen,
-            'CurrentWeight_Kg': CurrentWeight_Kg,
-            'CurrentHeight_cm': CurrentHeight_cm,
-            'Sex_Female': Sex_Female,
-            'Sex_Male': Sex_Male,
-            'Biometric_Status_No': Biometric_Status_No,
-            'Biometric_Status_Yes': Biometric_Status_Yes,
-            'ViralLoadIndication_Initial': ViralLoadIndication_Initial,
-            'ViralLoadIndication_Normal_priority_status': ViralLoadIndication_Normal_priority_status,
-            'Appointment_Status_Active_With_Drugs': Appointment_Status_Active_With_Drugs,
-            'Appointment_Status_LTFU': Appointment_Status_LTFU,
-            'Appointment_Status_Missed_Appointment': Appointment_Status_Missed_Appointment}
+    CurrentWeight_Kg = st.sidebar.slider('CurrentWeight_Kg', float(X.CurrentWeight_Kg.min()), float(X.CurrentWeight_Kg.max()), float(X.CurrentWeight_Kg.mean()))
+    Age_At_Start = st.sidebar.slider('Age_At_Start', int(X.Age_At_Start.min()), int(X.Age_At_Start.max()), int(X.Age_At_Start.mean()))
+    current_Age = st.sidebar.slider('current_Age', int(X.current_Age.min()), int(X.current_Age.max()), int(X.current_Age.mean()))
+
+    # Categorical Features (using selectbox for unique categorical options)
+    Appointment_Status_LTFU = st.sidebar.selectbox('Appointment_Status_LTFU', X.Appointment_Status_LTFU.unique())
+    Appointment_Status_Active_With_Drugs = st.sidebar.selectbox('Appointment_Status_Active_With_Drugs', X.Appointment_Status_Active_With_Drugs.unique())
+    Biometric_Status_No = st.sidebar.selectbox('Biometric_Status_No', X.Biometric_Status_No.unique())
+    CurrentARTRegimen = st.sidebar.selectbox('CurrentARTRegimen', X.CurrentARTRegimen.unique())
+    Biometric_Status_Yes = st.sidebar.selectbox('Biometric_Status_Yes', X.Biometric_Status_Yes.unique())
+    RegimenAtARTStart = st.sidebar.selectbox('RegimenAtARTStart', X.RegimenAtARTStart.unique())
+    ViralLoadIndication_Initial = st.sidebar.selectbox('ViralLoadIndication_Initial', X.ViralLoadIndication_Initial.unique())
+    ViralLoadIndication_Normal_priority_status = st.sidebar.selectbox('ViralLoadIndication_Normal_priority_status', X.ViralLoadIndication_Normal_priority_status.unique())
+    Appointment_Status_Missed_Appointment = st.sidebar.selectbox('Appointment_Status_Missed_Appointment', X.Appointment_Status_Missed_Appointment.unique())
+
+    # Categorical Features - Gender
+    Sex_Female = st.sidebar.selectbox('Sex_Female', X.Sex_Female.unique())
+    Sex_Male = st.sidebar.selectbox('Sex_Male', X.Sex_Male.unique())
+
+    # Create a data dictionary with the features in the specified order
+    data = {
+        'Days_To_Schedule': Days_To_Schedule,
+        'Appointment_Status_LTFU': Appointment_Status_LTFU,
+        'CurrentViralLoad': CurrentViralLoad,
+        'Appointment_Status_Active_With_Drugs': Appointment_Status_Active_With_Drugs,
+        'Biometric_Status_No': Biometric_Status_No,
+        'DaysOfARVRefill': DaysOfARVRefill,
+        'CurrentHeight_cm': CurrentHeight_cm,
+        'CurrentARTRegimen': CurrentARTRegimen,
+        'Biometric_Status_Yes': Biometric_Status_Yes,
+        'CurrentWeight_Kg': CurrentWeight_Kg,
+        'RegimenAtARTStart': RegimenAtARTStart,
+        'Age_At_Start': Age_At_Start,
+        'current_Age': current_Age,
+        'ViralLoadIndication_Initial': ViralLoadIndication_Initial,
+        'ViralLoadIndication_Normal_priority_status': ViralLoadIndication_Normal_priority_status,
+        'Appointment_Status_Missed_Appointment': Appointment_Status_Missed_Appointment,
+        'Sex_Female': Sex_Female,
+        'Sex_Male': Sex_Male
+    }
+
+    # Convert to dataframe
     features = pd.DataFrame(data, index=[0])
     return features
+
+
+
 
 df = user_input_features()
 
@@ -66,7 +84,7 @@ st.write(df)
 st.write('---')
 
 # Load the pre-trained model
-with open('ART_RF_Model.pkl', 'rb') as f:
+with open('new_ART_RF_Model1.pkl', 'rb') as f:
     model = pickle.load(f)
 
 # Apply Model to Make Prediction
